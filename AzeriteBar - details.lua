4cb6cae31889b0b6bda9d2ccf4e91bd0fb523698
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-05-30 22:33:40 +0300

    bar add function is now private, so just find bar through full enumeration

--- AzeriteBar - details.lua
+++ AzeriteBar - details.lua
@@ -1,3 +1,5 @@
+local a_name, a_env = ...
+
 local function UpdateOverlayFrameText_details(self)
    local text = self.OverlayFrame.Text
    if not text:IsShown() then return end
@@ -9,17 +11,7 @@ local function UpdateOverlayFrameText_details(self)
    text:SetFormattedText("%s | Left: %d", text:GetText(), totalLevelXP - xp)
 end
 
--- Won't work:
--- hooksecurefunc(AzeriteBarMixin, "UpdateOverlayFrameText", UpdateOverlayFrameText_details)
--- templates grabbed their own ref to function from Mixin already, so we'll hook bar addition and scan bars to find copies instead
-
-local function hook_AzeriteBar()
-   for idx, bar in pairs(StatusTrackingBarManager.bars) do
-      -- print("BAR", bar.UpdateOverlayFrameText == AzeriteBarMixin.UpdateOverlayFrameText)
-      if bar.UpdateOverlayFrameText == AzeriteBarMixin.UpdateOverlayFrameText then
-         hooksecurefunc(bar, "UpdateOverlayFrameText", UpdateOverlayFrameText_details)
-      end
-   end
+local azerite_bar = a_env.FindFrameByPredicate(function(frame) return frame.UpdateOverlayFrameText == AzeriteBarMixin.UpdateOverlayFrameText end)
+if azerite_bar then
+   hooksecurefunc(azerite_bar, "UpdateOverlayFrameText", UpdateOverlayFrameText_details)
 end
-
-hooksecurefunc(StatusTrackingBarManager, "AddBarFromTemplate", hook_AzeriteBar)

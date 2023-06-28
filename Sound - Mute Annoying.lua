489f6501246e5603c0752add903c24b665d5603c
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-06-28 12:50:01 +0300

    mute Grummle vendor sounds - they really get on my nerves when constantly using Yak mount

---
+++ Sound - Mute Annoying.lua
@@ -0,0 +1,18 @@
+for _, soundID in ipairs({
+   640336, -- VO_Grummle_Standard_Vendor_01.ogg
+   640338, -- VO_Grummle_Standard_Vendor_02.ogg
+   640340, -- VO_Grummle_Standard_Vendor_03.ogg
+   640314, -- VO_Grummle_Standard_Farewell_01.ogg
+   640316, -- VO_Grummle_Standard_Farewell_02.ogg
+   640318, -- VO_Grummle_Standard_Farewell_03.ogg
+   640320, -- VO_Grummle_Standard_Farewell_04.ogg
+   640180, -- VO_Grummle_Kooky_Vendor_01.ogg
+   640182, -- VO_Grummle_Kooky_Vendor_02.ogg
+   640184, -- VO_Grummle_Kooky_Vendor_03.ogg
+   640158, -- VO_Grummle_Kooky_Farewell_01.ogg
+   640160, -- VO_Grummle_Kooky_Farewell_02.ogg
+   640162, -- VO_Grummle_Kooky_Farewell_03.ogg
+   640164, -- VO_Grummle_Kooky_Farewell_04.ogg
+}) do
+   MuteSoundFile(soundID)
+end

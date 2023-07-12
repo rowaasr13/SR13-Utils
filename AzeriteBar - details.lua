4cf0b872b2be83ffb77f4f1344b1ac9bc5f4944b
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-07-12 15:02:59 +0300

    switch to 100105 direct bar hooking in main/secondary containers and force update after hook to display details immediately

--- AzeriteBar - details.lua
+++ AzeriteBar - details.lua
@@ -6,12 +6,22 @@ local function UpdateOverlayFrameText_details(self)
 
    local bar = self.StatusBar
    local xp = bar:GetAnimatedValue();
-   local _, totalLevelXP = bar:GetMinMaxValues();
+   local _, totalLevelXP = bar:GetMinMaxValues()
 
    text:SetFormattedText("%s | Left: %d", text:GetText(), totalLevelXP - xp)
 end
 
-local azerite_bar = a_env.FindFrameByPredicate(function(frame) return frame.UpdateOverlayFrameText == AzeriteBarMixin.UpdateOverlayFrameText end)
-if azerite_bar then
-   hooksecurefunc(azerite_bar, "UpdateOverlayFrameText", UpdateOverlayFrameText_details)
+local function HookAzeriteBar(self)
+   local bars = self.bars
+   for idx = 1, #bars do
+      local frame = bars[idx]
+      if frame.UpdateOverlayFrameText == AzeriteBarMixin.UpdateOverlayFrameText then
+         hooksecurefunc(frame, "UpdateOverlayFrameText", UpdateOverlayFrameText_details)
+         frame:UpdateTextVisibility()
+         frame:Update()
+      end
+   end
 end
+
+HookAzeriteBar(MainStatusTrackingBarContainer)
+HookAzeriteBar(SecondaryStatusTrackingBarContainer)

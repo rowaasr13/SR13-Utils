573d231390dce675275bc66a0181955db13637bc
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2023-09-23 08:06:50 +0300

    reorganize/localize stuff, LibTTScan works again

--- MerchantFrame - filter useless.lua
+++ MerchantFrame - filter useless.lua
@@ -1,16 +1,20 @@
+local IsMerchantItemAlreadyKnown = LibStub("LibTTScan-1.0").IsMerchantItemAlreadyKnown
+local item_icon_color = CreateColor(0.6, 0.2, 0.2)
+local item_name_text_color = CreateColor(0.6, 0.2, 0.2)
+
 local function ColorFilterMerchantItems()
    for i=1, MERCHANT_ITEMS_PER_PAGE do
       local index = (((MerchantFrame.page - 1) * MERCHANT_ITEMS_PER_PAGE) + i)
-      if LibStub("LibTTScan-1.0").IsMerchantItemAlreadyKnown(index) then
+      if IsMerchantItemAlreadyKnown(index) then
          local merchantButton = _G["MerchantItem" .. i]
-         SetItemButtonSlotVertexColor     (merchantButton, 0.4, 0, 0);
+         -- SetItemButtonSlotVertexColor     (merchantButton, 0.4, 0, 0);
 
-         -- local nameFrame = _G["MerchantItem" .. i .. "NameFrame"]
-         -- SetItemButtonNameFrameVertexColor(merchantButton, 0.8, 0, 0);
+         local nameText = _G["MerchantItem" .. i .. "Name"]
+	 nameText:SetText(item_name_text_color:WrapTextInColorCode(nameText:GetText()))
 
          local itemButton = _G["MerchantItem" .. i .. "ItemButton"]
-         SetItemButtonTextureVertexColor      (itemButton, 0.4, 0, 0);
-         SetItemButtonNormalTextureVertexColor(itemButton, 0.4, 0, 0);
+         SetItemButtonTextureVertexColor      (itemButton, item_icon_color:GetRGB())
+         SetItemButtonNormalTextureVertexColor(itemButton, item_icon_color:GetRGB())
       end
    end
 end

be2bf70f3f15d5bd0ec6f2493210ced0fdf65874
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2025-05-05 23:48:37 +0300

    /aucue command - make undermine.exchange link from current Auctionator item; reuse parts of previous command as GetAuctionatorItemInfo

--- Auctionator - Slash Commands.lua
+++ Auctionator - Slash Commands.lua
@@ -1,9 +1,15 @@
 local a_name, a_env = ...
 
+local function GetAuctionatorItemInfo()
+   local item_info =     AuctionatorSellingFrame
+   item_info = item_info.SaleItemFrame
+   item_info = item_info.itemInfo
+
+   return item_info
+end
+
 local function RegisterToSellWithTC()
-   local item_info =      AuctionatorSellingFrame
-   item_info = item_info. SaleItemFrame
-   item_info = item_info. itemInfo
+   local item_info = GetAuctionatorItemInfo()
    if not item_info then return end
 
    if item_info.itemID then
@@ -12,4 +18,16 @@ local function RegisterToSellWithTC()
       _G['SR13.exports'].TrashCompactor.UpdateList()
    end
 end
-a_env.register_slash("tc_sell_10m", { "/tcsauc" }, RegisterToSellWithTC)
+a_env.register_slash("tc_sell_session", { "/tcsauc" }, RegisterToSellWithTC)
+
+local function MakeUndermineKazzakLink()
+   local item_info = GetAuctionatorItemInfo()
+   if not item_info then return end
+
+   if item_info.itemID then
+      local link = 'https://undermine.exchange/#eu-kazzak/' .. item_info.itemID
+      _G["SR13-Lib"]["SR13-CopyPasta"].copypasta_popup("EU-Kazzak", link)
+   end
+end
+a_env.register_slash("copypasta_auctionator_undermine_kazzak", { "/aucue" }, MakeUndermineKazzakLink)
+
